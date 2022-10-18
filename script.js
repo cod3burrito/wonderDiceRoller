345bf3fd8e74a89fc2193ce0e9af4b2ab85a46c8
WonderBurrito <robintanner44@gmail.com>
2022-10-18 18:40:35 -0400

    updating the style and script

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ console.log(Math.floor(Math.random() * 8) + 1);
 console.log(Math.floor(Math.random() * 6) + 1);
 
 console.log(Math.floor(Math.random() * 4) + 1);
+
+
+const button = document.querySelectorAll("button");
+
+document.addEventListener("click", function rollD20 () {
+    return (Math.floor(Math.random() * 20)+ 1);
+    if (return === 20) {
+        alert("Critical success!");
+    } else if (return === 1) {
+        alert("Shit.")
+    } else {
+        document.appendChild(return, "p");
+    }
+});
+
+
+// // Other dice functions
+// function rollD12 () {}
+// function rollD10 () {}
+// function rollD8 () {}
+// function rollD6 () {}
+// function rollD4 () {}
