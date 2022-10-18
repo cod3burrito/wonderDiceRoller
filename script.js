f3394a07340c880436bbe03eca48c3f728488bda
cod3burrito <robintanner44@gmail.com>
2022-10-18 16:00:02 -0400

    wrote more code

---
+++ script.js
@@ -0,0 +1,12 @@
+// roll a d 20
+console.log(Math.floor(Math.random() * 20) + 1);
+
+console.log(Math.floor(Math.random() * 12) + 1);
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+console.log(Math.floor(Math.random() * 8) + 1);
+
+console.log(Math.floor(Math.random() * 6) + 1);
+
+console.log(Math.floor(Math.random() * 4) + 1);
